15bd5a2fdef172cf47da1adc61d7957fe783b80a
Daniil Tolkachev <94922898+deniman23@users.noreply.github.com>
2025-07-11 11:14:07 +0300

    fix to deploy

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+until psql "$DATABASE_URL" -c '\q'; do
+  echo "⏳ waiting for postgres…"
+  sleep 2
+done
+
+goose -dir /migrations postgres "$DATABASE_URL" up
+exec su-exec postgres /locator
